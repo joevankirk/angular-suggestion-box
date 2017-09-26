ff8cb6ea9cf510ada3f9b25c09fe77da58f453ea
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-26 16:35:36 -0500

    Add demo comments to suggestions.js

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -4,22 +4,22 @@ app.factory('suggestions', [function(){
     {
       title: 'End all club emails with Laffy Taffy jokes',
       upvotes: 9,
-      comments: [],
+      comments: ['I love laffy taff', 'your mom loves laffy taffy'],
     },
     {
       title: 'Free pizza at club meetings',
       upvotes: 15,
-      comments: [],
+      comments: ["I'm in!", "Sorry I'm on a diet but I'll bring salad"],
     },
     {
       title: 'Retrofit water fountain with Gatorade',
       upvotes: 7,
-      comments: [],
+      comments: ["Mr. Deeds, yes, please!", "Gatorade...water...gatorade!"],
     },
     {
       title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
       upvotes: 3,
-      comments: [],
+      comments: ['woahhhhoooohhhh', "He was great in the beatles. Cool song"],
     },
     ]
   };
