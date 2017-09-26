05c657439ca810b03a5c23e3d965335e7d2e506b
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-26 17:01:06 -0500

    Refactor home view and add routing to app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,11 @@
 var app = angular.module('SuggestionBox', ['ngRoute']);
 
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
       controller: 'HomeController',
       templateUrl: 'views/home.html'
     })
-    .when('/:id', {
-      controller: 'CommentController',
-      templateUrl: 'views/comment.html'
-  })
     .otherwise({
       redirectTo: '/'
     });
