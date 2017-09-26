71a8a0b209def9343547440fb160dc74b6d2acd4
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-26 17:14:06 -0500

    Logic for suggestion controller to create and upvote comments

--- js/controllers/SuggestionController.js
+++ js/controllers/SuggestionController.js
@@ -1,1 +1,20 @@
-SuggestionController.js
+app.controller('SuggestionController', ['$scope', '$routeParams', 'suggestions', function($scope, $routeParams, suggestions) {
+  $scope.post = suggestions.posts[$routeParams.id];
+
+  $scope.addComment = function() {
+    //if input empty, don't submit
+    if(!$scope.body || $scope.body === "") {
+      return;
+    }
+
+    //push comments in suggestions.js
+    $scope.posts.comments.push({
+      body: $scope.body,
+      upvotes: 0,
+    });
+  };
+
+  $scope.upVote = function(comment) {
+    comment.upvotes += 1;
+  }
+}]);
