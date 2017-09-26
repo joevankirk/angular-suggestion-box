2cd7a0932411c1876a5efdd63c3f6c09113e69e4
Joe Van Kirk <imjoevankirk@gmail.com>
2017-09-26 16:06:53 -0500

    Sort posts by upvotes

--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -2,13 +2,13 @@ app.factory('suggestions', [function(){
   var demoSuggestions = {
     posts: [
     {
-      title: 'Free pizza at club meetings',
-      upvotes: 15,
+      title: 'End all club emails with Laffy Taffy jokes',
+      upvotes: 9,
       comments: [],
     },
     {
-      title: 'End all club emails with Laffy Taffy jokes',
-      upvotes: 9,
+      title: 'Free pizza at club meetings',
+      upvotes: 15,
       comments: [],
     },
     {
